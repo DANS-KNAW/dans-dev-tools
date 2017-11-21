0701c56a4794bd1732bfe92926f86b090f0b4ffb
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2017-11-21 18:04:10 +0100

    Added run-quiet.sh to run without maven output

--- run-quiet.sh
+++ run-quiet.sh
@@ -15,27 +15,4 @@
 # limitations under the License.
 #
 
-APPHOME=home
-
-if [ -z $QUIET ]; then
-    QUIET=""
-fi
-
-if [ -z $LOGBACK_OPTS ]; then
-    LOGBACK_OPTS=""
-fi
-
-if [ -z $LOGBACK_CONFIG ]; then
-    LOGBACK_CONFIG=$APPHOME/cfg/logback.xml
-fi
-
-
-for a in "$@"
-do
-  ARGS="$ARGS'$a' "
-done
-
-LC_ALL=en_US.UTF-8 \
-mvn exec:java $QUIET -Dapp.home=$APPHOME \
-                     -Dlogback.configurationFile=$LOGBACK_CONFIG \
-                     -Dexec.args="$ARGS" $LOGBACK_OPTS
+QUIET="-q" run.sh $@
