ac3c46e8b81ad061fb170a3f2a9aa5afc43b6ab8
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2018-08-19 20:24:21 +0200

    WIP

---
+++ build-install-springfield-mod.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if (( $# < 2 )); then
+    echo "Installs springfield module into local Maven cache"
+    echo
+    echo "Usage: $(basename $0) <artifact> <version>"
+    echo "Example: $(basename $0) war/momar.war 1.0.0"
+    exit 1
+fi
+
+ARTIFACT=$1
+VERSION=$2
+BASENAME=$(basename $ARTIFACT)
+MODULE=$(echo $BASENAME | cut -d. -f1)
+EXT=$(echo $BASENAME | cut -d. -f2)
+SPRINGFIELD_PACKAGE=springfield2
+
+mvn install:install-file -Dfile=$ARTIFACT -DartifactId=$MODULE -Dpackaging=$EXT -DgroupId=nl.knaw.dans.springfield2 -Dversion=$VERSION
