93854eb2c5fb3ca35be12562ce193d892ceb55e5
Jan van Mansum <janvanmansum@gmail.com>
2021-09-29 22:00:58 +0200

    Added remote debugging settings to start-* scripts

---
+++ start-debug.sh
@@ -0,0 +1,2 @@
+MAVEN_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=y" \
+start.sh $@
--- start-service.sh
+++ start-service.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 set -x
-
+MAVEN_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n" \
 start.sh server etc/config.yml
